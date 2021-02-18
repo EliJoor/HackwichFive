cc4f9180552ea8ebbe32d5020caf2a714c4c3617
EliJoor <77301046+EliJoor@users.noreply.github.com>
2021-02-18 10:12:14 -1000

    Completed Parts 1-7

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //declare and assign a global var
+    var currentIndex = 0
+    
+    var favoriteFoodsArray = ["Pizza","Sushi","Ribs","Lamb","Ice cream"]
+    
+    @IBOutlet weak var topLable: UILabel!
+    
+    @IBOutlet weak var bottomLable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topLable.text = "My Favorite Foods"
+        bottomLable.text = favoriteFoodsArray[currentIndex]
     }
 
 
